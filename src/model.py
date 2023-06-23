6a8d8580a45214ab01d0e9add35c3edef1112690
igorkf <igorkuivjogi@hotmail.com>
2023-06-23 17:40:50 -0500

    fix resnet in_channels

--- src/model.py
+++ src/model.py
@@ -1,3 +1,4 @@
+import torch
 from torch import nn
 from torchvision import models
 
@@ -17,18 +18,22 @@ from torchvision import models
 
 
 class ResNet18(nn.Module):
-    def __init__(self, bands, in_channels=3):
+    def __init__(self, bands):
         super(ResNet18, self).__init__()
 
         self.model = models.resnet18()
+        # first layer is nn.Conv2d(3, 64, kernel_size=(7, 7), stride=(2, 2), padding=(3, 3), bias=False)
+        self.model.conv1 = nn.Conv2d(bands, 64, kernel_size=(7, 7), stride=(2, 2), padding=(3, 3), bias=False)
 
-        # original definition of the first layer on the renset class
-        # self.conv1 = nn.Conv2d(3, 64, kernel_size=7, stride=2, padding=3, bias=False)
-
-        self.conv0 = nn.Conv2d(bands, in_channels, 1)
         num_ftrs = self.model.fc.in_features
         self.model.fc = nn.Linear(num_ftrs, 3)  # output 3 classes
 
+        # learnable band weights
+        # ws = torch.randint(0, 2, (bands, )).float()  # naturals on {0, 1}
+        # ws = torch.rand(bands)  # reals on [0, 1)
+        # self.W = torch.nn.Parameter(ws, requires_grad=True)
+
     def forward(self, x):
-        x = self.conv0(x)
+        # channel-wise multiplication (https://discuss.pytorch.org/t/how-to-channel-wise-multiply-an-matrix-and-a-vector/61670)
+        # x = x * self.W.unsqueeze(dim=-1).unsqueeze(dim=-1)
         return self.model(x)
