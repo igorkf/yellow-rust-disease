cf582d89509b67af48fe422c7dc0eb9eb55eb4ea
igorkf <igorkuivjogi@hotmail.com>
2023-06-24 23:22:10 -0500

    add submission (lb=0.62916)

--- src/predict_nn.py
+++ src/predict_nn.py
@@ -4,7 +4,6 @@ from collections import Counter
 import torch
 from torch import nn
 from torch.utils.data import DataLoader
-from sklearn.model_selection import StratifiedKFold
 import numpy as np
 
 from preprocessing import make_df
@@ -12,26 +11,19 @@ from utils import set_seed
 from dataset import TIFDataset
 from model import ResNet18
 
-from training import accuracy
-
 
 PATH = 'data'
 LABEL2INT = {'Health': 0, 'Other': 1, 'Rust': 2}
 INT2LABEL = {0: 'Health', 1: 'Other', 2: 'Rust'}
 BANDS = 125
-RANDOM_BANDS = BANDS // 2
-IN_CHANNELS = 3
-N_FOLDS = 5
 RANDOM_SEED = 42
 BATCH_SIZE = 64
 NUM_WORKERS = 4
-EPOCHS = 100
 MODELS = [
-    'resnet18_fold0_epoch61_acc0.709821',
-    'resnet18_fold1_epoch51_acc0.765625',
-    'resnet18_fold2_epoch46_acc0.707589',
-    'resnet18_fold3_epoch78_acc0.695312',
-    'resnet18_fold4_epoch15_acc0.684152'
+    '20230624_195734_fold0_epoch385_acc0.723214',
+    '20230624_200822_fold1_epoch22_acc0.744420',
+    '20230624_211024_fold2_epoch305_acc0.742188',
+    '20230624_215235_fold3_epoch336_acc0.723214',
 ]
 
 
@@ -45,43 +37,23 @@ if __name__ == '__main__':
     )
     print(f'Device:', device)
 
-    df = make_df(PATH, test=False)
-    df['label'] = df['label'].map(LABEL2INT)
     df_test = make_df(PATH, test=True)
     df_test['label'] = df_test['label'].map(LABEL2INT)   
     test_ds = TIFDataset(df_test, bands=BANDS, transform=None)
     test_dataloader = DataLoader(test_ds, batch_size=BATCH_SIZE, num_workers=NUM_WORKERS, shuffle=False) 
 
-    test_probs = np.zeros((len(df_test), 3))
-    acc_folds = np.zeros(N_FOLDS)
-    skf = StratifiedKFold(n_splits=N_FOLDS, shuffle=True, random_state=RANDOM_SEED)
-    for fold, (_, val) in enumerate(skf.split(df.drop('label', axis=1), df['label'])):
-        print(f'\n-------- FOLD {fold} --------')
-
-        df_val = df.loc[val]
-        val_ds = TIFDataset(df_val, bands=BANDS, transform=None)
-        val_dataloader = DataLoader(val_ds, batch_size=BATCH_SIZE, num_workers=NUM_WORKERS, shuffle=False)
+    test_probs = []
+    acc_folds = [float(x.split('acc')[-1]) for x in MODELS]
+    for model_name in MODELS:
+        print(f'\n-------- {model_name} --------')
 
         # define model
-        model = ResNet18(BANDS, IN_CHANNELS)
-        model_path = os.path.join('output', 'models', MODELS[fold])
+        model = ResNet18(BANDS)
+        model_path = os.path.join('output', 'models', model_name)
         model.load_state_dict(torch.load(model_path, map_location=torch.device(device)))
         model = model.to(device)
         model.eval()
-
-        # predict on val
-        total_acc = 0.0
-        with torch.no_grad():
-            for idx, batch in enumerate(val_dataloader):
-                x = batch[0].to(device).float()
-                ytrue = batch[1].to(device)
-                ypred = model(x)
-                acc = accuracy(ypred, ytrue)
-                total_acc += acc
-        avg_acc = total_acc / (idx + 1)
-        print(f'[VAL] Acc: {avg_acc:.6f}')
-        acc_folds[fold] = avg_acc
-
+        
         # predict on test
         probs = []
         with torch.no_grad():
@@ -91,13 +63,17 @@ if __name__ == '__main__':
                 p = nn.functional.softmax(ypred, dim=1).cpu().numpy()
                 probs.append(p)
         probs = np.concatenate(probs)
-        test_probs += probs / len(MODELS)  # blend
+        test_probs.append(probs)
 
-    print('-' * 30)
-    print('Fold results:')
-    print('ACC:', acc_folds.round(6))
-    print('Mean ACC:', acc_folds.mean(), 'Std ACC:', acc_folds.std())
-
+    # blend
+    print('weights:', acc_folds)
+    acc = np.mean(acc_folds)
+    print('mean w:', acc, 'std w:', np.std(acc_folds))
+    test_probs = np.average(
+        np.array(test_probs),
+        weights=acc_folds,
+        axis=0
+    ) 
     print(test_probs.shape, test_probs.min(), test_probs.max())
     preds_idx = test_probs.argmax(axis=1)
     print(Counter(preds_idx))
@@ -108,6 +84,5 @@ if __name__ == '__main__':
     df_test['label'] = preds
     df_test['path'] = df_test['path'].str.split('/').str[-1]
     df_test = df_test.rename(columns={'path': 'Id', 'label': 'Category'})
-    df_test.to_csv('output/submission.csv', index=False)
-
+    df_test.to_csv(f'output/submission_acc{acc}.csv', index=False)
  
--- src/train_nn.py
+++ src/train_nn.py
@@ -1,7 +1,8 @@
 from datetime import datetime
+import copy
 
+import pandas as pd
 import torch
-from torch import nn
 from torch.utils.data import DataLoader
 from torch.optim import lr_scheduler
 # from torch.utils.tensorboard import SummaryWriter
@@ -23,12 +24,11 @@ LABEL2INT = {'Health': 0, 'Other': 1, 'Rust': 2}
 INT2LABEL = {0: 'Health', 1: 'Other', 2: 'Rust'}
 BANDS = 125
 RANDOM_BANDS = BANDS // 2
-IN_CHANNELS = 3
 N_FOLDS = 5
 RANDOM_SEED = 42
 BATCH_SIZE = 64
 NUM_WORKERS = 4
-EPOCHS = 100
+EPOCHS = 500
 
 
 if __name__ == '__main__':
@@ -43,7 +43,6 @@ if __name__ == '__main__':
 
     df = make_df(PATH, test=False)
     df['label'] = df['label'].map(LABEL2INT)
-    # df_test = make_df(PATH, test=True)
     
     acc_folds = np.zeros(N_FOLDS)
     skf = StratifiedKFold(n_splits=N_FOLDS, shuffle=True, random_state=RANDOM_SEED)
@@ -77,34 +76,47 @@ if __name__ == '__main__':
         val_dataloader = DataLoader(val_ds, batch_size=BATCH_SIZE, num_workers=NUM_WORKERS, shuffle=False)
 
         # define model
-        model = ResNet18(BANDS, in_channels=IN_CHANNELS)
+        model = ResNet18(BANDS)
         model = model.to(device)
 
         loss_fn = torch.nn.CrossEntropyLoss()
-        # optimizer = torch.optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
-        optimizer = torch.optim.Adam(model.parameters(), lr=0.001)        
+        optimizer = torch.optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
+        # optimizer = torch.optim.Adam(model.parameters(), lr=0.001)        
 
         # https://www.kaggle.com/code/isbhargav/guide-to-pytorch-learning-rate-scheduling
         # scheduler = lr_scheduler.StepLR(optimizer, step_size=20, gamma=0.1, verbose=True)
-        scheduler = lr_scheduler.CosineAnnealingLR(optimizer, T_max=10, eta_min=0, verbose=True)
+        # scheduler = lr_scheduler.CosineAnnealingLR(optimizer, T_max=10, eta_min=0, verbose=True)
+        # scheduler = lr_scheduler.ReduceLROnPlateau(optimizer, mode='max', factor=0.1, patience=20, verbose=True)
+        scheduler = lr_scheduler.CosineAnnealingWarmRestarts(optimizer, T_0=10, T_mult=1, eta_min=0, verbose=True)
 
         best_acc = 0.0
         for epoch in range(EPOCHS):
             train_loss, train_acc = train_one_epoch(model, train_dataloader, loss_fn, optimizer, device)
             val_loss, val_acc = validate_one_epoch(model, val_dataloader, loss_fn, device)
-
-            print(f'\n[EPOCH] {epoch + 1}/{EPOCHS}')
-            print(f'[TRAIN] Loss: {train_loss:.6f}, Acc: {train_acc:.6f}')
-            print(f'[VAL  ] Loss: {val_loss:.6f}, Acc: {val_acc:.6f}')
-
             scheduler.step()
 
             if val_acc > best_acc:
                 best_acc = val_acc
-                model_path = f'output/models/resnet18_fold{fold}_epoch{epoch + 1}_acc{val_acc:.6f}'
-                print('Saving:', model_path)
-                torch.save(model.state_dict(), model_path)
+                best_epoch = epoch
+                best_model = copy.deepcopy(model)
+                timestamp = datetime.now().strftime('%Y%m%d_%H%M%S')
+                best_model_path = f'output/models/{timestamp}_fold{fold}_epoch{epoch + 1}_acc{val_acc:.6f}'
+
+            print(f'\n[EPOCH] {epoch + 1}/{EPOCHS}')
+            print(f'[TRAIN] Loss: {train_loss:.6f}, Acc: {train_acc:.6f}')
+            print(f'[VAL  ] Loss: {val_loss:.6f}, Acc: {val_acc:.6f}')
+            print(f'[BEST VAL] Epoch: {best_epoch + 1}, Acc: {best_acc:.6f}')
         
+            # print learnable band weights
+            # for name, param in model.named_parameters():
+            #     if name == 'W':
+            #         w = param.data.cpu().numpy()
+            #         print('Learnable band weights:')
+            #         print(pd.DataFrame(w).describe().round(4).T)
+        
+        print('Saving:', best_model_path)
+        torch.save(best_model.state_dict(), best_model_path)
+
         acc_folds[fold] = best_acc
 
     print('-' * 30)
